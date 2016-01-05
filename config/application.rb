c866b19491992b6b5f1118a81fd04c7d11e7668e
mizuki <mikkie.914@gmail.com>
2016-01-05 17:04:00 +0900

    use i18n for setting rails to Japanese

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -24,3 +23,5 @@ module Freecanvas
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
+
+config.i18n.default_locale = :ja
