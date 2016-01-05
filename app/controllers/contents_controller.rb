8f567f95fe3f80f51e34015b080195f323a56cde
mizuki <mikkie.914@gmail.com>
2016-01-05 21:38:51 +0900

    create contents_controller`s pattern

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,2 +1,20 @@
 class ContentsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destoroy
+  end
 end
