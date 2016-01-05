ab3fe28c87c37a001b8cbb57c474c9d1f0929f5c
mizuki <mikkie.914@gmail.com>
2016-01-05 21:21:27 +0900

    delete project root

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
  devise_for :users
  root 'top#index'
  get '/' => 'top#index'
- resources :projects
- resources :contents, :except => [:index]
+ resources :contents
 
 end
