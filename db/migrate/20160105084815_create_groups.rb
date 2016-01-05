5a9b0df554d832bfe01537193fce55c1df2076da
mizuki <mikkie.914@gmail.com>
2016-01-05 21:49:08 +0900

    delete group model

--- db/migrate/20160105084815_create_groups.rb
+++
@@ -1,9 +0,0 @@
-class CreateGroups < ActiveRecord::Migration
-  def change
-    create_table :groups do |t|
-      t.string :key, unique: true
-      t.text :detail
-      t.timestamps null: false
-    end
-  end
-end
