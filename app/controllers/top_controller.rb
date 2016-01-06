34ab6b1c313816b94a4c17ef1928c33e03ca101e
mizuki <mikkie.914@gmail.com>
2016-01-06 16:33:54 +0900

    correct top#index

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,7 +2,7 @@ class TopController < ApplicationController
 
   def index
     # contentsテーブルから最新順に10コンテンツ取得
-    @contents = Content.order('id ASC').limit(10)
+    @contents = Content.all.order('id ASC').limit(10)
   end
 
 end
