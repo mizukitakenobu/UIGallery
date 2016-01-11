d7a191f7fdb9c94713fa8e50c7f7ab0e8c94d555
mizuki <mikkie.914@gmail.com>
2016-01-11 11:38:50 +0900

    define association between content and image

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,3 +1,3 @@
 class Content < ActiveRecord::Base
-  mount_uploader :image, ImageUploader
+  has_many :images
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,4 @@
 class Image < ActiveRecord::Base
+  belongs_to :content
+  mount_uploader :image, ImageUploader
 end
