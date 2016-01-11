6b056982aeada7da65fcc5201b1add228a8fa6f3
mizuki <mikkie.914@gmail.com>
2016-01-11 11:56:24 +0900

    make image_creative save on image model

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,3 +1,14 @@
 class Content < ActiveRecord::Base
   has_many :images
+  before_validation :image_creative_to_id, if: :has_image_creative?
+
+  private
+  def has_image_creative
+    image_creative.present?
+  end
+
+  def image_creative_to_id
+    image = Image.where(creative: image_creative).first_or_create
+    self.image_id = image.id
+  end
 end
