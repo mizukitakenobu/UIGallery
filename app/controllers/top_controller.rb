c9b33c4f2a04772966aace13cf74be6eef67a7fd
mizuki <mikkie.914@gmail.com>
2016-01-11 17:24:10 +0900

    edit top

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,6 +3,7 @@ class TopController < ApplicationController
   def index
     # contentsテーブルから最新順に10コンテンツ取得
     @contents = Content.all.order('id DESC').limit(10)
+    @images = Image.where(content_id: params[:id])
   end
 
 end
