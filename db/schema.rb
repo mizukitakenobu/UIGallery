b0addf9e4e09c706c9b02a678ad3f4d01347d4a1
mizuki <mikkie.914@gmail.com>
2016-01-11 13:54:48 +0900

    add content_id on image table

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111025026) do
+ActiveRecord::Schema.define(version: 20160111041532) do
 
   create_table "contents", force: :cascade do |t|
     t.string   "title",       limit: 255
     t.text     "description", limit: 65535
-    t.text     "image",       limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "image_id",    limit: 4
   end
 
   create_table "images", force: :cascade do |t|
@@ -27,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160111025026) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "creative",   limit: 65535
+    t.integer  "content_id", limit: 4
   end
 
   create_table "users", force: :cascade do |t|
