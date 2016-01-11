e1cbe937f48e21adabbf845e1b35adb70c822d60
mizuki <mikkie.914@gmail.com>
2016-01-11 13:56:16 +0900

    add attr_accessor :images, :creative, :caption on content model

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,15 +1,5 @@
 class Content < ActiveRecord::Base
+  attr_accessor :images, :creative, :caption
   has_many :images
   accepts_nested_attributes_for :images
-  before_validation :image_creative_to_id, if: :has_image_creative?
-
-  private
-  def has_image_creative
-    image_creative.present?
-  end
-
-  def image_creative_to_id
-    image = Image.where(creative: image_creative).first_or_create
-    self.image_id = image.id
-  end
 end
