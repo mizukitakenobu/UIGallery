c00fd8bffef00573333e8f3b38c8ef4c025529c6
mizuki <mikkie.914@gmail.com>
2016-01-11 18:59:10 +0900

    rename usercontroller to Userscontroller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+def show
+  @user = User.find(params[:id])
+  @content = Content.where(user_id: params[:id])
+end
+
+def edit
+  @user = User.find(params[:id])
+end
+
+def update
+  @user = User.find(params[:id])
+  if current_user.id == @user.id
+    @user.update(user_params)
+  end
+  redirect_to @user
+end
+
+private
+def user_params
+  params.require(:user).permit(:email, :encrypted_password, :username)
+end
+end
