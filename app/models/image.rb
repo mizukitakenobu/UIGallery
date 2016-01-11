c63f6b7e93f715691bbd55ccfcb2b7e239e06bbc
mizuki <mikkie.914@gmail.com>
2016-01-11 18:58:10 +0900

    delete image model

--- app/models/image.rb
+++
@@ -1,4 +0,0 @@
-class Image < ActiveRecord::Base
-  belongs_to :content
-  mount_uploader :image, ImageUploader
-end
--- db/migrate/20160111022253_create_images.rb
+++
@@ -1,9 +0,0 @@
-class CreateImages < ActiveRecord::Migration
-  def change
-    create_table :images do |t|
-      t.text :image
-      t.text :caption
-      t.timestamps
-    end
-  end
-end
