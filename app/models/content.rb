a385e46ee12bb827d2e5f0e9302fc28faca68145
mizuki <mikkie.914@gmail.com>
2016-01-11 14:27:57 +0900

    add attr_accessor :creative_cache

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,5 +1,5 @@
 class Content < ActiveRecord::Base
-  attr_accessor :images, :creative, :caption
+  attr_accessor :images, :creative, :caption, :creative_cache
   has_many :images
   accepts_nested_attributes_for :images
 end
