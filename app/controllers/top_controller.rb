d1da9133fd5923837cb672c265cf8d4827356d35
mizuki <mikkie.914@gmail.com>
2016-01-12 17:09:03 +0900

    set for kaminari

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,7 +2,7 @@ class TopController < ApplicationController
 
   def index
     # contentsテーブルから最新順に10コンテンツ取得
-    @contents = Content.all.order('id DESC').limit(10)
+    @contents = Content.all.order('id DESC').page(params[:page]).limit(10)
     @thumbnails = Thumbnail.where(content_id: params[:id])
   end
 
