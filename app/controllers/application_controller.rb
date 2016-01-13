a63db2bbe8843c676f33b6900f42678e3ddaea32
mizuki <mikkie.914@gmail.com>
2016-01-13 18:14:17 +0900

    delete before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_in) << :group_key
-    devise_parameter_sanitizer.for(:sign_up) << :group_key
-    devise_parameter_sanitizer.for(:account_update) << :group_key
-  end
 end
