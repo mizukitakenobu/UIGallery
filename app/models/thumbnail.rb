8dc631d5cbf55e463cb3460f0ab17474d77726e8
mizuki <mikkie.914@gmail.com>
2016-01-13 21:36:23 +0900

    validate creative size #14

--- app/models/thumbnail.rb
+++ app/models/thumbnail.rb
@@ -1,4 +1,15 @@
 class Thumbnail < ActiveRecord::Base
   belongs_to :content
   mount_uploader :creative, ImageUploader
+
+  validate :file_size
+
+    # 5MB以上のファイルはUPLOADできないようにしてみる
+  def file_size
+      upload_limit = 3.megabytes.to_i
+      if creative.file.size > upload_limit
+        errors.add(:avater, "のサイズが大きすぎます。")
+      end
+    end
 end
+
