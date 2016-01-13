b150e2c9b6bd21ad4576ea5145a9e0dcd7f003c5
mizuki <mikkie.914@gmail.com>
2016-01-13 21:37:30 +0900

    make uploading Japanese name files

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => 'IwW0cij7MWjX+QK8S37M6qUBiQMaHHq130cwTLTU',
     :region                 => 'ap-northeast-1'
   }
-
   config.fog_directory = 'uigallery'
 
 end
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
