91b4953cadd94f612ba8fa25251b78627f7046f0
mizuki <mikkie.914@gmail.com>
2016-01-15 00:00:05 +0900

    delete awskey

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAIUQGXOEIQKJAOF2A',
-    :aws_secret_access_key  => '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',
+    :aws_access_key_id      => '',
+    :aws_secret_access_key  => '',
     :region                 => 'ap-northeast-1'
   }
   config.fog_directory = 'uigallery'
