2e24287d831b99a522458b6becfea6e42ad600aa
mizuki <mikkie.914@gmail.com>
2016-01-15 00:19:48 +0900

    no message

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => 'hogehoge',
-    :aws_secret_access_key  => 'fugafuga',
+    :aws_access_key_id      => '',
+    :aws_secret_access_key  => '',
     :region                 => 'ap-northeast-1'
   }
   config.fog_directory = 'uigallery'
