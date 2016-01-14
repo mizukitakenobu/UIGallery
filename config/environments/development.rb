953c8b2679c8644d1d38306a8bfc0fc613285105
mizuki <mikkie.914@gmail.com>
2016-01-15 00:59:12 +0900

    no message

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,8 +22,8 @@ Rails.application.configure do
     :address => "smtp.gmail.com",
     :port => 587,
     :domain => 'smtp.gmail.com',
-    :user_name => "takenobu.dev@gmail.com", #ご自身のgmailアドレス
-    :password => "Mamikkie6218", #ご自身のgmailアドレスのパスワード
+    :user_name => "", #ご自身のgmailアドレス
+    :password => "", #ご自身のgmailアドレスのパスワード
     :authentication => 'login',
   }
 
