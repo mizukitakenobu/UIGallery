cb7ed08156046defff870e0e59a9cbf44a087d65
mizuki <mikkie.914@gmail.com>
2016-01-15 14:31:56 +0900

    ooo

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
  devise_for :users
  root 'top#index'
- get '/' => 'top#index'
  resources :contents, :except =>[:index]
  resources :users, only: [:show, :edit, :update]
 end
