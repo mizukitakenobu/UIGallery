d047057a913bdddf2b3473e0947d7ee584943842
mizuki <mikkie.914@gmail.com>
2016-01-15 16:56:28 +0900

    Add config to keep aws key secret

--- app/models/thumbnail.rb
+++ app/models/thumbnail.rb
@@ -1,6 +1,5 @@
 class Thumbnail < ActiveRecord::Base
   belongs_to :content
   mount_uploader :creative, ImageUploader
-
 end
 
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,11 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => Settings[:aws][:access_key_id],
+    :aws_secret_access_key  => Settings[:aws][:secret_access_key],
+    :region                 => Settings[:aws][:region]
+  }
+  config.fog_directory = 'uigallery'
+
+end
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
